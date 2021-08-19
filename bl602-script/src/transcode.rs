4f8f580821f86ae16e4cf998585cc218f2309426
lupyuen <luppy@appkaki.com>
2021-08-19 13:17:26 +0800

    Transcoding

--- bl602-script/src/transcode.rs
+++ bl602-script/src/transcode.rs
@@ -14,6 +14,50 @@ fn transcode_node(node: &[rhai::ASTNode]) -> bool {
         if COUNT > 5 { return false }
     }    
     println!("Node: {:#?}", node);
+
+    /* TOOD: Stmt(
+        Var(
+            11 @ 4:24,
+            "LED_GPIO" @ 4:13,
+            (),
+            4:9,
+        ),
+    ),
+    Expr(
+        11 @ 4:24,
+    )
+    becomes...
+    ( let* 
+        (( LED_GPIO 11 ))
+        ...
+    ) */
+
+    /* TODO: Stmt(
+        FnCall(
+            FnCallExpr {
+                namespace: Some(
+                    gpio,
+                ),
+                hashes: 4301736447638837139,
+                args: [
+                    Variable(LED_GPIO #1) @ 7:29,
+                    StackSlot(0) @ 7:39,
+                    StackSlot(1) @ 7:42,
+                ],
+                constants: [
+                    0,
+                    0,
+                ],
+                name: "enable_output",
+                capture: false,
+            },
+            7:15,
+        ),
+    )
+    becomes...
+    ( bl_gpio_enable_output 11 0 0 )
+    */   
+
     true
 }
 
